a65a32a2497a3e455e50c1f58a8b14514c0b22ea
Yongjin <yongjin5352@gmail.com>
2024-06-04 20:29:51 +0900

    feat: implement feature #1

--- prisoners.py
+++ prisoners.py
@@ -3,3 +3,9 @@ from random import shuffle
 def find_num():
     # 죄수 번호가 들어갈 박스
     boxes = {}
+    # 죄수 번호(1~100)를 셔플.
+    nums = [i for i in range(1, 101)]
+    shuffle(nums)
+
+    # 앞 죄수가 열어본 박스
+    visited = set()
