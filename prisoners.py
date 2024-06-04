76e3f3e7b3d4322953b136b68040785be42595e0
bokyeong <wntjdals0412@gmail.com>
2024-06-04 20:31:12 +0900

    Feat: 죄수 번호 분배

--- prisoners.py
+++ prisoners.py
@@ -3,3 +3,10 @@ from random import shuffle
 def find_num():
     # 죄수 번호가 들어갈 박스
     boxes = {}
+
+    # 셔플한 수 저장할 배열
+    nums = []
+    # 2
+    # 셔플된 죄수 번호를 하나씩 뽑아 박스에 넣는다.
+    for i, num in enumerate(nums):
+        boxes[i + 1] = num
